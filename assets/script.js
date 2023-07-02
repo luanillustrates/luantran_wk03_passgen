7ec105ee0db3b862712d52e7383426d769a51255
luanillustrates <luanillustrates@gmail.com>
2023-07-02 21:28:44 +0930

    update to js with generating pw

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,21 @@
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
 // variables for password criteria
 
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numeric = '0123456789';
 var special = '!@#$%^&*()_+=-';
+var password = "";
+var pwCriteria = "";
 
-passwordLower = lowercase.split('');
-passwordUpper = uppercase.split('');
-passwordNumeric = numeric.split('');
-passwordSpecial = special.split('');
 
 // function to generate checklist for password
 
-function createPassword() {
+function generatePassword() {
 
   var passLower = confirm("Would you like any lowercase letters?");
   var passUpper = confirm("Would you like any uppercase letters?");
@@ -28,28 +31,44 @@ function createPassword() {
 
   else if (passLower === false && passUpper === false && passNumeric === false && passSpecial === false) {
     alert("Please select from the password criteria");
-    var passLower = confirm("Would you like any lowercase letters?");
-    var passUpper = confirm("Would you like any uppercase letters?");
-    var passNumeric = confirm("Would you like any numbers?");
-    var passSpecial = confirm("Would you like any special characters?");
-    var passLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+  }
+
+  // allocating the selections to add to password
+
+  if (passLower) {
+    pwCriteria += lowercase;
+  }
+
+  if (passUpper) {
+    pwCriteria += uppercase;
+  }
+
+  if (passNumeric) {
+    pwCriteria += numeric;
   }
   
+  if (passSpecial) {
+    pwCriteria += special;
+  }
+
+  for (var i = 0; i < passLength; i++) {
+    password += pwCriteria[Math.floor(Math.random() * pwCriteria.length)];
+  }
+  
+  console.log(password);  
+
 };
 
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", createPassword);
+generateBtn.addEventListener("click", generatePassword);
